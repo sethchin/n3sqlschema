5b118bdeb8e3af96e4f243d091fe41bd02d91ab1
sethchin <36907585+sethchin@users.noreply.github.com>
2018-10-03 09:30:27 +0800

    Add files via upload

--- n3_table_creation.sql
+++ n3_table_creation.sql
@@ -1,5 +1,3 @@
-
--- Drop Tables 
 DROP TABLE IF EXISTS Address, API, Bank_Account, Borrower, Branch_Address, Users, Business_Entity, Business_Entity_Attribute,
 Business_User_Role, Component, Country, Currency, Financing_History, Funder, Gov_Entity_Reference, Log_File, Permission, Role, 
 Role_Permission, system_config, User_Role, Financing;
@@ -19,7 +17,7 @@ Deleted   BOOLEAN,
 Preffered_Language VARCHAR(35),
 Profile_Pic TEXT,
 IsLock BOOLEAN,	
-CONSTRAINT Users_PK PRIMARY KEY (User_ID, Business_Entity_ID),
+CONSTRAINT Users_ID_PK PRIMARY KEY (User_ID),
 CONSTRAINT Users_User_ID_key UNIQUE (User_ID));	
 	 	 	  
 CREATE TABLE IF NOT EXISTS Business_User_Role (
@@ -28,7 +26,7 @@ User_ID	INTEGER NOT NULL,
 Business_Entity_ID INTEGER NOT NULL,
 Business_User_Name	VARCHAR(200),
 Component_Names	VARCHAR(200),	
-CONSTRAINT Business_User_Role_PK PRIMARY KEY(Business_User_Role_ID, User_ID, Business_Entity_ID),
+CONSTRAINT Business_User_Role_ID_PK PRIMARY KEY(Business_User_Role_ID),
 CONSTRAINT Business_User_Role_Key UNIQUE(Business_User_Role_ID));
 
 
@@ -36,7 +34,7 @@ CREATE TABLE IF NOT EXISTS User_Role (
 User_Role_ID  SERIAL NOT NULL,
 Business_Entity_ID INTEGER NOT NULL,
 Role_ID INTEGER NOT NULL,	
-CONSTRAINT User_Role_PK PRIMARY KEY (User_Role_ID, Business_Entity_ID, Role_ID), 
+CONSTRAINT User_Role_ID_PK PRIMARY KEY (User_Role_ID), 
 CONSTRAINT User_Role_Key UNIQUE(User_Role_ID)); 
  
 
@@ -55,7 +53,7 @@ Branch_Address_ID INTEGER,
 Bank_Account_ID	INTEGER,	
 Business_Logo TEXT,
 Borrower BOOLEAN,
-CONSTRAINT Business_Entity_PK PRIMARY KEY(Business_Entity_ID, Address_ID, Branch_Address_ID, Bank_Account_ID),
+CONSTRAINT Business_Entity_ID_PK PRIMARY KEY(Business_Entity_ID),
 CONSTRAINT Business_Entity_key UNIQUE(Business_Entity_ID));
 
 
@@ -66,7 +64,7 @@ Business_Entity_ID	INTEGER NOT NULL,
 Attribute_id VARCHAR(200),
 Attribute_name VARCHAR(200),
 Attribute_value VARCHAR(200),
-CONSTRAINT Business_Entity_Attribute_PK PRIMARY KEY(Business_Entity_Attribute_ID, Business_Entity_ID),
+CONSTRAINT Business_Entity_Attribute__IDPK PRIMARY KEY(Business_Entity_Attribute_ID),
 CONSTRAINT Business_Entity_Attribute_key UNIQUE(Business_Entity_Attribute_ID));
 
  
@@ -78,7 +76,7 @@ Additional_Street_Name VARCHAR(65),
 City_Name VARCHAR(65),
 Postal_Zone VARCHAR(35),
 Country_Subentity VARCHAR(65),
-CONSTRAINT Address_PK PRIMARY KEY(Address_ID),
+CONSTRAINT Address_ID_PK PRIMARY KEY(Address_ID),
 CONSTRAINT Address_key UNIQUE(Address_ID));
 
  
@@ -90,7 +88,7 @@ Additional_Street_Name VARCHAR(65),
 City_Name VARCHAR(65),
 Postal_Zone VARCHAR(35),
 Country_Subentity VARCHAR(65), 
-CONSTRAINT Branch_Address_PK PRIMARY KEY(Branch_Address_ID),
+CONSTRAINT Branch_Address_ID_PK PRIMARY KEY(Branch_Address_ID),
 CONSTRAINT Branch_Address_key UNIQUE(Branch_Address_ID));
 
 
@@ -118,7 +116,7 @@ Capitalize_Interest	  DECIMAL(18,2),
 Roll_over_effective_date VARCHAR(10),
 Roll_over_Maturity_date VARCHAR(10),
 Overdue_Amt			 DECIMAL (18,2),	
-CONSTRAINT BORROWER_PK PRIMARY KEY(Borrower_Business_Entity_ID),
+CONSTRAINT BORROWER_ID_PK PRIMARY KEY(Borrower_Business_Entity_ID),
 CONSTRAINT BORROWER_Key UNIQUE(Borrower_Business_Entity_ID));
 
 
@@ -148,7 +146,7 @@ Dynamic_Discount_Amt DECIMAL(18,2),
 Discount_Expiration_Date VARCHAR(10),
 Other_Charges DECIMAL(18,2),
 Total_Charges DECIMAL(18,2),	
-CONSTRAINT FINANCING_KEY PRIMARY KEY(Financing_ID, Funder_Business_Entity_ID, Invoice_ID),
+CONSTRAINT FINANCING_ID_KEY PRIMARY KEY(Financing_ID),
 CONSTRAINT FINANCING_PK UNIQUE(Financing_ID));		
 						   
 							   
@@ -171,7 +169,7 @@ Original_Loan_Amt	DECIMAL(18,2),
 Interest_Accrued_Amt DECIMAL(18,2),
 Over_Due_Amt DECIMAL(18,2),
 Date_Time TIMESTAMP,	
-CONSTRAINT FINANCING_HISTORY_KEY PRIMARY KEY(Financing_History_ID, Funder_Business_Entity_ID, Financing_ID, Invoice_ID),
+CONSTRAINT FINANCING_HISTORY_ID_KEY PRIMARY KEY(Financing_History_ID),
 CONSTRAINT FINANCING_HISTORY_PK UNIQUE(Financing_History_ID));		
  
 
@@ -186,13 +184,13 @@ System_comment				VARCHAR(200),
 System_Date_Time				TIMESTAMP	 	
 );
 
-
+ 									   
 
 CREATE TABLE IF NOT EXISTS Funder
 (
-Business_Entity_ID SERIAL NOT NULL,
-CONSTRAINT Funder_Key PRIMARY KEY(Business_Entity_ID),
-CONSTRAINT Funder_PK UNIQUE(Business_Entity_ID)	
+Funder_Business_Entity_ID SERIAL NOT NULL,
+CONSTRAINT Funder_ID_Key PRIMARY KEY(Funder_Business_Entity_ID),
+CONSTRAINT Funder_PK UNIQUE(Funder_Business_Entity_ID)	
 );
  
  
@@ -206,7 +204,7 @@ Bank_BIC VARCHAR(35),
 Currency_ID INTEGER,
 Country_ID INTEGER,
 Address_ID INTEGER,	
-CONSTRAINT Bank_Account_Key	PRIMARY KEY(Bank_Account_ID,Business_Entity_ID, Currency_ID, Country_ID, Address_ID),
+CONSTRAINT Bank_Account_ID_Key	PRIMARY KEY(Bank_Account_ID),
 CONSTRAINT Bank_Account_PK UNIQUE(Business_Entity_ID)		
 );
 
@@ -222,7 +220,7 @@ Log_Result BOOLEAN,
 Log_Action VARCHAR(35),
 Log_Field VARCHAR(100),
 Log_Date_Time TIMESTAMP,	
-CONSTRAINT Log_File_Key PRIMARY KEY(Log_ID),
+CONSTRAINT Log_File_ID_Key PRIMARY KEY(Log_ID),
 CONSTRAINT Log_File_PK UNIQUE(Log_ID) );
  
 		
@@ -239,19 +237,19 @@ UEN_Issue_Date	VARCHAR(10),
 Reg_Street_Name	VARCHAR(100),
 Reg_Postal_Code	VARCHAR(100),
 Country VARCHAR(20),	
-CONSTRAINT GOV_ENTITY_REFERENCE_Key	PRIMARY KEY(GOV_ENTITY_REFERENCE_ID),
+CONSTRAINT GOV_ENTITY_REFERENCE_ID_Key	PRIMARY KEY(GOV_ENTITY_REFERENCE_ID),
 CONSTRAINT GOV_ENTITY_REFERENCE_PK UNIQUE(GOV_ENTITY_REFERENCE_ID)	
 );
 
- 
+ 							   
 CREATE TABLE IF NOT EXISTS  Country(   
 Country_ID SERIAL NOT NULL,
 Country_Name	VARCHAR(255),
 Country_Code VARCHAR(3) NOT NULL,
 Dial_Code VARCHAR(5),
 Dial_Sub_Code VARCHAR(30),
-CONSTRAINT Country_Key PRIMARY KEY(Country_ID,Country_Code),
-CONSTRAINT Country_PK UNIQUE(Country_ID)	
+CONSTRAINT Country_ID_Key PRIMARY KEY(Country_ID),
+CONSTRAINT Country_PK UNIQUE(Country_ID)
 );
 
 
@@ -260,14 +258,15 @@ CREATE TYPE category_method AS ENUM (
   'Menu');
   
 CREATE TABLE IF NOT EXISTS Component (	
-   Component_ID VARCHAR(35) NOT NULL,
-   Reference_ID VARCHAR(35) NOT NULL,
+   Component_ID SERIAL NOT NULL,
+   Reference_ID INTEGER NOT NULL,
    Category category_method,
-   Permission_ID VARCHAR(35) NOT NULL,
-CONSTRAINT Component_Key PRIMARY KEY (Component_ID, Reference_ID, Permission_ID),
+   Permission_ID INTEGER NOT NULL,
+CONSTRAINT Component_ID_Key PRIMARY KEY (Component_ID),
 CONSTRAINT Component_PK UNIQUE(Component_ID)
 ); 
-  
+ 
+ 									   
 CREATE TYPE permission_method AS ENUM (
   'Create', 
   'Update',
@@ -276,47 +275,72 @@ CREATE TYPE permission_method AS ENUM (
   
   
 CREATE TABLE IF NOT EXISTS Permission (
-   Permission_ID VARCHAR(35) NOT NULL,
+   Permission_ID SERIAL NOT NULL,
    Permission_name permission_method,
    Permission_Description VARCHAR(200),
-CONSTRAINT Permission_Key PRIMARY KEY(Permission_ID),
+CONSTRAINT Permission_ID_Key PRIMARY KEY(Permission_ID),
 CONSTRAINT Permission_PK Unique(Permission_ID)	
 ); 
    
-  
-  
+ 
+ 
 CREATE TABLE IF NOT EXISTS Role (
-Role_ID	 VARCHAR(35) NOT NULL,
+Role_ID	 SERIAL NOT NULL,
 Role_Name	VARCHAR(200),
-CONSTRAINT Role_Key PRIMARY KEY(Role_ID),
+CONSTRAINT Role_ID_Key PRIMARY KEY(Role_ID),
 CONSTRAINT Role_PK Unique(Role_ID)	
 );  
   
-  
+ 
 CREATE TABLE IF NOT EXISTS Role_Permission ( 
-Role_Permission_Id	VARCHAR(35) NOT NULL,
-Role_ID				VARCHAR(35) NOT NULL,
-Permission_ID		VARCHAR(35) NOT NULL,
-CONSTRAINT Role_Permission_Key PRIMARY KEY(Role_Permission_Id, Role_ID, Permission_ID),
+Role_Permission_Id	SERIAL NOT NULL,
+Role_ID				SERIAL NOT NULL,
+Permission_ID		SERIAL NOT NULL,
+CONSTRAINT Role_Permission_ID_Key PRIMARY KEY(Role_Permission_Id),
 CONSTRAINT Role_Permission_PK Unique(Role_Permission_Id));
 
 
+CREATE TABLE IF NOT EXISTS Currency (  								 
+Currency_ID  SERIAL	NOT NULL,
+Currency_Name	VARCHAR(255),
+Currency_code_alpha	VARCHAR(3),
+Currency_code_numeric	NUMERIC(3),
+CONSTRAINT Currency_ID_Key PRIMARY KEY(Currency_Id),
+CONSTRAINT Currency_PK Unique(Currency_Id));	
+ 									 
+  
 CREATE TABLE IF NOT EXISTS API (
-API_ID VARCHAR(35) NOT NULL,
+API_ID SERIAL NOT NULL,
 API_Name VARCHAR(100),
 URL	 VARCHAR(200),
 Description	VARCHAR(200),
-CONSTRAINT API_Key PRIMARY KEY(API_ID),	
+CONSTRAINT API_ID_Key PRIMARY KEY(API_ID),	
 CONSTRAINT API_PK Unique(API_ID)	
 );
 
+							  
+
+CREATE TABLE IF NOT EXISTS Menu ( 							  
+menu_id	SERIAL NOT NULL,
+menu_name	 VARCHAR(100),
+parent_id	 VARCHAR(35),
+Child		boolean,	
+URI		VARCHAR(255),
+CONSTRAINT menu_id_key PRIMARY KEY(menu_id),	
+CONSTRAINT menu_id_PK Unique(menu_id)	
+);							  
+							  
+							  
+							  
 									 
 CREATE TYPE Invoice_method AS ENUM (
   'Pending', 
   'Approved',
   'Reject',
   'Paid');									 
-									 
+
+drop table invoice							  
+							  
 CREATE TABLE IF NOT EXISTS Invoice(
 Invoice_ID SERIAL NOT NULL,
 Invoice_Number	VARCHAR(35),
@@ -335,10 +359,10 @@ Payment_Reference_Number VARCHAR(35),
 Address_Id INTEGER NOT NULL,
 Buyer_Business_Entity_Id	INTEGER NOT NULL,
 Supplier_Business_Entity_Id  INTEGER NOT NULL,
-CONSTRAINT Invoice_Key PRIMARY KEY(Invoice_ID, Currency_Id, Address_Id, Buyer_Business_Entity_Id, Supplier_Business_Entity_Id),	
+CONSTRAINT Invoice_ID_Key PRIMARY KEY(Invoice_ID),	
 CONSTRAINT Invoice_PK Unique(Invoice_ID)	
 );									 
-									 
+ 				  
 CREATE TABLE IF NOT EXISTS Invoice_Line_Item(									 
 Line_Item_ID	SERIAL NOT NULL,
 Invoice_ID		INTEGER NOT NULL,
@@ -347,11 +371,11 @@ Description		VARCHAR(255),
 Quantity		NUMERIC(5),
 Unit_Price		DECIMAL(18,2),
 Line_Item_Amount DECIMAL(18,2),
-CONSTRAINT Invoice_Line_Item_Key PRIMARY KEY(Line_Item_ID, Invoice_Id),	
+CONSTRAINT Invoice_Line_Item_ID_Key PRIMARY KEY(Line_Item_ID),	
 CONSTRAINT Invoice_Line_Item_PK Unique(Line_Item_ID)		
 );
 
-
+ 
 CREATE TABLE IF NOT EXISTS Goods_Origination(
 Goods_ID	SERIAL NOT NULL,
 Invoice_ID	INTEGER NOT NULL,
@@ -360,7 +384,7 @@ Port_Of_Discharge VARCHAR(200),
 Shipment_Type	 VARCHAR(200),
 Goods_Information	VARCHAR(200),
 Other_Parties_Information	VARCHAR(200),
-CONSTRAINT Goods_Origination_Key PRIMARY KEY(Goods_ID, Invoice_Id),	
+CONSTRAINT Goods_Origination_ID_Key PRIMARY KEY(Goods_ID),	
 CONSTRAINT Goods_Origination_PK Unique(Goods_ID)		
 );
 									 
@@ -375,8 +399,98 @@ CONSTRAINT Goods_Origination_PK Unique(Goods_ID)
  
 
 ALTER TABLE Users
-ADD CONSTRAINT Users_Business_Entity_ID_fkey FOREIGN KEY (Business_Entity_ID) REFERENCES business_entity (business_entity_id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;
+ADD CONSTRAINT Business_Entity_ID_fkey FOREIGN KEY (Business_Entity_ID) REFERENCES business_entity (business_entity_id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;
 
  
- 
+ALTER TABLE Business_User_Role
+ADD CONSTRAINT 	Business_Entity_ID_fkey FOREIGN KEY (Business_Entity_ID) REFERENCES business_entity (business_entity_id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;
+								 
 
+ALTER TABLE Business_User_Role
+ADD CONSTRAINT User_ID_fkey FOREIGN KEY(User_ID) REFERENCES Users (User_ID) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+ALTER TABLE User_Role
+ADD CONSTRAINT Business_Entity_ID_fkey FOREIGN KEY (Business_Entity_ID) REFERENCES business_entity (business_entity_id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;
+									 
+ALTER TABLE User_Role									 
+ADD CONSTRAINT Role_ID_fkey FOREIGN KEY (Role_ID) REFERENCES Role (Role_id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;										 
+  
+ALTER TABLE Business_Entity
+ADD CONSTRAINT Address_ID_fkey FOREIGN KEY (Address_ID) REFERENCES Address (Address_ID)	MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;								 
+			 					 
+ALTER TABLE Business_Entity_Attribute									 
+ADD CONSTRAINT Business_Entity_ID_fkey FOREIGN KEY (Business_Entity_ID) REFERENCES business_entity (business_entity_id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;
+									 
+									 
+ALTER TABLE BORROWER
+ADD CONSTRAINT Borrower_Business_Entity_ID_fkey FOREIGN KEY (Borrower_Business_Entity_ID) REFERENCES business_entity (business_entity_id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;
+									  									 
+ALTER TABLE Funder
+ADD CONSTRAINT Funder_Business_Entity_ID_fkey FOREIGN KEY (Funder_Business_Entity_ID) REFERENCES business_entity (business_entity_id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;
+ 
+									 
+ALTER TABLE Financing
+ADD CONSTRAINT Funder_Business_Entity_ID_fkey FOREIGN KEY (Funder_Business_Entity_ID) REFERENCES Funder (Funder_business_entity_id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;
+									 
+ALTER TABLE Financing									 
+ADD CONSTRAINT Invoice_ID_fkey FOREIGN KEY(Invoice_ID) REFERENCES Invoice(Invoice_ID) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;
+									 
+									 
+ALTER TABLE FINANCING_HISTORY
+ADD CONSTRAINT Financing_ID_fkey FOREIGN KEY(Financing_ID) REFERENCES Financing(Financing_ID) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;	
+									 
+ALTER TABLE FINANCING_HISTORY									 
+ADD CONSTRAINT Invoice_ID_fkey FOREIGN KEY(Invoice_ID) REFERENCES Invoice(Invoice_ID) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;
+									 
+ALTER TABLE FINANCING_HISTORY									 
+ADD CONSTRAINT Funder_Business_Entity_ID_fkey FOREIGN KEY (Funder_Business_Entity_ID) REFERENCES Funder (Funder_business_entity_id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;
+								 
+
+ALTER TABLE Bank_Account
+ADD CONSTRAINT Business_Entity_ID_Fkey FOREIGN KEY(Business_Entity_ID) REFERENCES business_entity (business_entity_id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;
+							  
+ALTER TABLE Bank_Account
+ADD CONSTRAINT Currency_ID_Fkey FOREIGN KEY(Currency_ID) REFERENCES currency (Currency_ID) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;
+							  
+ALTER TABLE Bank_Account
+ADD CONSTRAINT Address_ID_Fkey FOREIGN KEY(Address_ID) REFERENCES Address (Address_ID) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE Api
+ADD CONSTRAINT API_ID_Fkey FOREIGN KEY(API_ID) REFERENCES API (API_ID) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;
+							  
+ALTER TABLE Menu
+ADD CONSTRAINT Menu_ID_Fkey FOREIGN KEY(Menu_ID) REFERENCES Menu (Menu_ID) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;
+
+							  
+ALTER TABLE Component
+ADD CONSTRAINT Reference_ID_Fkey FOREIGN KEY(Reference_ID) REFERENCES API (API_ID) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;							  
+
+ALTER TABLE Component
+ADD CONSTRAINT Permission_ID_Fkey FOREIGN KEY(Permission_ID) REFERENCES Permission (Permission_ID) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;							  
+  
+ALTER TABLE Role_Permission									 
+ADD CONSTRAINT Permission_ID_Fkey FOREIGN KEY(Permission_ID) REFERENCES Permission (Permission_ID) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;							  
+
+ALTER TABLE Role_Permission									 
+ADD CONSTRAINT Role_ID_Fkey FOREIGN KEY(Role_ID) REFERENCES Role (Role_ID) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;							  
+							  
+ALTER TABLE Invoice
+ADD CONSTRAINT Currency_Id_Fkey FOREIGN KEY(Currency_Id) REFERENCES Currency (Currency_Id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;							  
+
+ALTER TABLE Invoice
+ADD CONSTRAINT Address_Id_Fkey FOREIGN KEY(Address_Id) REFERENCES Address (Address_Id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;							  
+
+ALTER TABLE Invoice
+ADD CONSTRAINT Buyer_Business_Entity_Id_Fkey FOREIGN KEY(Buyer_Business_Entity_Id) REFERENCES Business_Entity(Business_Entity_Id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;							  
+							  
+ALTER TABLE Invoice
+ADD CONSTRAINT Supplier_Business_Entity_Id_Fkey FOREIGN KEY(Supplier_Business_Entity_Id) REFERENCES Business_Entity(Business_Entity_Id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;							  
+
+  
+ALTER TABLE Invoice_line_item
+ADD CONSTRAINT Invoice_ID_fkey FOREIGN KEY(Invoice_ID) REFERENCES Invoice(Invoice_ID) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;
+							  
+
+ALTER TABLE Goods_Origination
+ADD CONSTRAINT Invoice_ID_fkey FOREIGN KEY(Invoice_ID) REFERENCES Invoice(Invoice_ID) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE;
